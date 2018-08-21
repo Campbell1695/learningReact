c081adb669e6491d4f3345ee9597b625b5349f13
Reef Daddy <campbellsharif@gmail.com>
2018-08-21 11:25:20 -0400

    App now has working form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: [
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+
         {
           title: 'Business Website',
           category: 'Web Design'
@@ -19,14 +26,20 @@ class App extends Component {
           title: 'Eccomerence Shopping Cart',
           category: 'Web Development'
         }
-      ]
-    }
+
+    ]});
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects})
   }
 
   render() {
     return (
       <div className="App">
-        My App
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects}/>
       </div>
     );
